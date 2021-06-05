5b3f3937cc79383655fa087128a0057aa174fe24
choolooloo <819391306@qq.com>
2021-06-05 18:09:51 +0800

    飞行物模型以及控制

---
+++ Source/FlyGame/FlyGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlyGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLYGAME_API AFlyGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FlyGame/Private/Drone.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Drone.h"
+#include <Components/StaticMeshComponent.h>
+#include <Components/BoxComponent.h>
+#include <PhysicsEngine/PhysicsThrusterComponent.h>
+#include <Kismet/KismetMathLibrary.h>
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+
+// Sets default values
+ADrone::ADrone()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	OutCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("OutCollision"));
+	RootComponent = OutCollision;
+	OutCollision->SetBoxExtent(FVector(100.0f));
+	OutCollision->SetSimulatePhysics(true);
+	OutCollision->BodyInstance.bLockXRotation = true;
+	OutCollision->BodyInstance.bLockYRotation = true;
+	OutCollision->SetCollisionProfileName(TEXT("Pawn"));
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(OutCollision);
+
+	Paddle1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle1"));
+	Paddle2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle2"));
+	Paddle3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle3"));
+	Paddle4 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Paddle4"));
+	Paddle1->SetupAttachment(Mesh, TEXT("Paddle1"));
+	Paddle2->SetupAttachment(Mesh, TEXT("Paddle2"));
+	Paddle3->SetupAttachment(Mesh, TEXT("Paddle3"));
+	Paddle4->SetupAttachment(Mesh, TEXT("Paddle4"));
+
+	UpThruster = CreateDefaultSubobject<UPhysicsThrusterComponent>(TEXT("UpThruster"));
+	UpThruster->SetupAttachment(RootComponent);
+
+	//UE_LOG(LogTemp, Warning, TEXT("the value of mass:%i"), OutCollision->GetMass());
+	UpThruster->ThrustStrength = 980.0f;
+
+	UpThruster->SetAutoActivate(true);
+	UpThruster->SetWorldRotation(UKismetMathLibrary::MakeRotFromX(-this->GetActorUpVector()));
+
+	ForwardThruster = CreateDefaultSubobject<UPhysicsThrusterComponent>(TEXT("ForwardThruster"));
+	ForwardThruster->SetupAttachment(RootComponent);
+	ForwardThruster->ThrustStrength = 0.0f;
+	ForwardThruster->SetAutoActivate(true);
+	ForwardThruster->SetWorldRotation(UKismetMathLibrary::MakeRotFromX(-this->GetActorForwardVector()));
+
+	Arm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Arm"));
+	Arm->SetupAttachment(Mesh);
+	Arm->TargetArmLength = 500.0f;
+	//Arm->SetRelativeRotation(FRotator(GetInputAxisValue(TEXT("Mouse_Y")), GetInputAxisValue(TEXT("Mouse_X")), 0.0f));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(Arm);
+}
+
+// Called when the game starts or when spawned
+void ADrone::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ADrone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (GetInputAxisValue(TEXT("Lift")) == 0)
+	{
+		UpThruster->ThrustStrength = 980.0f;
+	}
+	if (GetInputAxisValue(TEXT("Forward")) == 0)
+	{
+		ForwardThruster->ThrustStrength = 0.0f;
+	}
+	Arm->AddRelativeRotation(FRotator(GetInputAxisValue(TEXT("Mouse_Y")), GetInputAxisValue(TEXT("Mouse_X")), 0.0f));
+}
+
+// Called to bind functionality to input
+void ADrone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("Lift"), this, &ADrone::Lift);
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &ADrone::Forward);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ADrone::Trun);
+	PlayerInputComponent->BindAxis(TEXT("Mouse_X"));
+	PlayerInputComponent->BindAxis(TEXT("Mouse_Y"));
+}
+
+void ADrone::Lift(float value)
+{
+	UpThruster->ThrustStrength += value * LiftAcc * GetWorld()->DeltaTimeSeconds;
+	UpThruster->ThrustStrength = FMath::Clamp(UpThruster->ThrustStrength, -LiftThrustMax, LiftThrustMax);
+}
+
+void ADrone::Forward(float value)
+{
+	ForwardThruster->ThrustStrength += value * ForwardAcc * GetWorld()->DeltaTimeSeconds;
+	ForwardThruster->ThrustStrength = FMath::Clamp(ForwardThruster->ThrustStrength, -ForwardThrustMax, ForwardThrustMax);
+}
+
+void ADrone::Trun(float value)
+{
+	OutCollision->AddTorqueInDegrees(-this->GetActorUpVector() * value * TurnStrength);
+}
+
